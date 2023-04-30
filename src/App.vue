e69beb738ac93ae68bd457119e37124a0d8d3f90
Lynnsey Martin <lmartin@hubspot.com>
2023-04-30 08:46:31 -0600

    init styles

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
+      <h1>Jogger Logger</h1>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
---
+++ src/components/LoginButton.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+function initOAuth() {
+  alert('Start OAuth');
+}
+</script>
+
+<template>
+  <button @click="initOAuth" class="tertiary medium">Login</button>
+</template>
+
+<style scoped>
+button {
+  margin: 16px 0;
+}
+</style>
