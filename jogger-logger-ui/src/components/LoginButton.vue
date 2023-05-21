394b96cd17f75896b1e305ec556294b46f22be27
Lynnsey Martin <lmartin@hubspot.com>
2023-05-21 13:39:55 -0400

    start oauth on login button

--- jogger-logger-ui/src/components/LoginButton.vue
+++ jogger-logger-ui/src/components/LoginButton.vue
@@ -1,17 +1,19 @@
 <script setup lang="ts">
-const CLIENT_ID = process.env.CLIENT_ID;
-const SCOPE = process.env.SCOPE;
-const CALLBACK = process.env.CALLBACK;
+const { VITE_CLIENT_ID, VITE_OAUTH_SCOPE, VITE_APP_DOMAIN, DEV } = import.meta.env;
 
-if (!CLIENT_ID) {
-  throw new Error(`No client id declared.`)
+if (DEV && !VITE_CLIENT_ID) {
+  throw new Error(`No client id declared. Given: ${VITE_CLIENT_ID}`)
 }
 
-const OAUTH_URL = `http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${CALLBACK}&approval_prompt=force&scope=${SCOPE}`
+const callback = encodeURI(`http://${DEV ? "localhost:5173" : VITE_APP_DOMAIN}/exchange_token`)
+const oauthUrl = `http://www.strava.com/oauth/authorize?client_id=${VITE_CLIENT_ID}&response_type=code&redirect_uri=${callback}&approval_prompt=force&scope=${VITE_OAUTH_SCOPE}`
+function initOAuth() {
+  window.open(oauthUrl, '_blank')
+}
 </script>
 
 <template>
-  <button :href="OAUTH_URL" class="tertiary medium">Login</button>
+  <button @click="initOAuth" class="tertiary medium">Login</button>
 </template>
 
 <style scoped>
