47fed134bb4140a6cd47f44cfc1e817c7cfab02f
Lynnsey Martin <lmartin@hubspot.com>
2023-05-21 13:04:06 -0400

    add oauth link

--- jogger-logger-ui/src/components/LoginButton.vue
+++ jogger-logger-ui/src/components/LoginButton.vue
@@ -1,11 +1,17 @@
 <script setup lang="ts">
-function initOAuth() {
-  alert('Start OAuth');
+const CLIENT_ID = process.env.CLIENT_ID;
+const SCOPE = process.env.SCOPE;
+const CALLBACK = process.env.CALLBACK;
+
+if (!CLIENT_ID) {
+  throw new Error(`No client id declared.`)
 }
+
+const OAUTH_URL = `http://www.strava.com/oauth/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${CALLBACK}&approval_prompt=force&scope=${SCOPE}`
 </script>
 
 <template>
-  <button @click="initOAuth" class="tertiary medium">Login</button>
+  <button :href="OAUTH_URL" class="tertiary medium">Login</button>
 </template>
 
 <style scoped>
