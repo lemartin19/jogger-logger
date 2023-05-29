ea29cf8bfbbbd77e34edc4379b87aa5003068011
Lynnsey Martin <lmartin@hubspot.com>
2023-05-29 13:30:18 -0400

    style about page more

--- jogger-logger-ui/src/views/AboutView.vue
+++ jogger-logger-ui/src/views/AboutView.vue
@@ -1,22 +1,28 @@
 <template>
   <v-container class="fill-height">
-    <v-spacer />
-    <v-col cols="6">
-      <v-row>
-        <h1>About</h1>
-        <p>
+    <v-row>
+      <v-spacer />
+      <v-col cols="6">
+        <h1 class="my-4">About</h1>
+        <p class="my-4">
           This is a personal project to make it easier to combine the historical tracking and
           analysis of Strava with an interface to plan your future training. Knowing how you've
           gotten to where you are is part of knowing how to get where you want to go, so this tool
           makes it easier to see your past and future training cycles all in one place! Get after it
-          and use it how you want!<br />
-          <br />
-          Have any feedback? Open an
-          <a href="https://github.com/lemartin19/jogger-logger/issues">issue in the GitHub repo</a>
-          and let me know what you think!
+          and use it how you want!
         </p>
-      </v-row>
-    </v-col>
-    <v-spacer />
+        <p class="my-4">Have any feedback? Let me know what you think!</p>
+        <v-row justify="center"
+          ><v-btn
+            variant="text"
+            href="https://github.com/lemartin19/jogger-logger/issues"
+            class="my-2"
+            append-icon="mdi-open-in-new"
+            >File an issue in GitHub</v-btn
+          >
+        </v-row>
+      </v-col>
+      <v-spacer />
+    </v-row>
   </v-container>
 </template>
