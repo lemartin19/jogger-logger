e1e3b42225584587be71f21b102b0c8a2c134aa6
Lynnsey Martin <lmartin@hubspot.com>
2023-06-04 20:00:52 -0400

    use getAuthCookie

--- jogger-logger-ui/src/components/app/AppSettings.vue
+++ jogger-logger-ui/src/components/app/AppSettings.vue
@@ -1,12 +1,12 @@
 <script lang="ts">
-import { getIsAuthed } from '@/stores/auth';
+import { getAuthCookie } from '@/stores/auth';
 import { eventBus, COOKIE_SET } from '@/EventBus';
 import LoginButton from './LoginButton.vue';
 import ThemeToggle from './ThemeToggle.vue';
 
 export default {
   data() {
-    const isAuthed = Boolean(getIsAuthed(this.$cookies));
+    const isAuthed = Boolean(getAuthCookie(this.$cookies));
     return { isAuthed };
   },
 
@@ -22,7 +22,7 @@ export default {
 
   methods: {
     setIsAuthed() {
-      this.isAuthed = getIsAuthed(this.$cookies);
+      this.isAuthed = Boolean(getAuthCookie(this.$cookies));
     },
   },
 };
