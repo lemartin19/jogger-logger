a1b4c81345dad9e8c0d4f3600f823daa1e38fd6b
Lynnsey Martin <lmartin@hubspot.com>
2023-06-24 18:36:17 -0400

    with accessibility links

--- jogger-logger-ui/src/themes.ts
+++ jogger-logger-ui/src/themes.ts
@@ -1,6 +1,7 @@
 import type { ThemeDefinition } from 'vuetify';
 
-// https://coolors.co/f3fffc-1c1c22-5d8a7a-bb8bb0-b93127-e7c34b-4c6dc7-3f7d3a
+// https://coolors.co/f3fffc-1c1c22-5d8a7a-bb8bb0-b93127-e7c34b-4c6dc7
+// accessibility: https://davidmathlogic.com/colorblind/#%23F3FFFC-%231C1C22-%235D8A7A-%23BB8BB0-%23B93127-%23E7C34B-%234C6DC7
 const light: ThemeDefinition = {
   dark: false,
   colors: {
@@ -13,11 +14,11 @@ const light: ThemeDefinition = {
     error: '#B93127', // Fire Brick
     warning: '#E7C34B', // Saffron
     info: '#4C6DC7', // Royal Blue (web-color)
-    success: '#3F7D3A', // Fern Green
   },
 };
 
-// https://coolors.co/1c1c22-f3fffc-bb8bb0-1e4844-972820-e6ba2a-3d68dd-1b8a3a
+// https://coolors.co/1c1c22-f3fffc-bb8bb0-1e4844-972820-e6ba2a-3d68dd
+// accessibility: https://davidmathlogic.com/colorblind/#%231C1C22-%23F3FFFC-%23BB8BB0-%231E4844-%23972820-%23E6BA2A-%233D68DD
 const dark: ThemeDefinition = {
   dark: true,
   colors: {
@@ -30,7 +31,6 @@ const dark: ThemeDefinition = {
     error: '#972820', // Burnt Umber
     warning: '#E6BA2A', // Saffron
     info: '#3D68DD', // Royal Blue (web-color)
-    success: '#1B8A3A', // Forest Green
   },
 };
 
