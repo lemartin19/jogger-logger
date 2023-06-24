8048cc7ca993144c7c0b524de6f38a8617ab1376
Lynnsey Martin <lmartin@hubspot.com>
2023-06-24 17:34:47 -0400

    center loading state on screen and only exchange with code

--- jogger-logger-ui/src/views/ExchangeOAuthToken.vue
+++ jogger-logger-ui/src/views/ExchangeOAuthToken.vue
@@ -18,43 +18,45 @@ export default {
 
     if (!code) {
       this.error = `Can't authorize to Strava without code parameter.`;
+    } else {
+      const serverEndpoint = `http://${
+        DEV ? 'localhost:5173/api' : VITE_SERVER
+      }/exchange?code=${code}`;
+      axios
+        .post(serverEndpoint)
+        .then(({ data }) => {
+          const profile = data['profile'];
+          const accessToken = data['access_token'];
+          const expiresIn = data['expires_in'];
+
+          if (!accessToken) {
+            throw new Error(`Auth unsuccessful, invalid access token or expiration time.`);
+          }
+          if (profile) {
+            this.authStore.athleteProfile = profile;
+          }
+
+          this.$cookies.set(VITE_AUTH_COOKIE, accessToken, expiresIn || 0);
+          eventBus.emit(COOKIE_SET);
+          this.$router.push('/');
+        })
+        .catch((e) => {
+          this.loading = false;
+          this.error = e.message;
+        });
     }
-
-    const serverEndpoint = `http://${
-      DEV ? 'localhost:5173/api' : VITE_SERVER
-    }/exchange?code=${code}`;
-    axios
-      .post(serverEndpoint)
-      .then(({ data }) => {
-        const profile = data['profile'];
-        const accessToken = data['access_token'];
-        const expiresIn = data['expires_in'];
-
-        if (!accessToken) {
-          throw new Error(`Auth unsuccessful, invalid access token or expiration time.`);
-        }
-        if (profile) {
-          this.authStore.athleteProfile = profile;
-        }
-
-        this.$cookies.set(VITE_AUTH_COOKIE, accessToken, expiresIn || 0);
-        eventBus.emit(COOKIE_SET);
-        this.$router.push('/');
-      })
-      .catch((e) => {
-        this.loading = false;
-        this.error = e.message;
-      });
   },
 };
 </script>
 
 <template>
-  <main>
-    <h2>Exchanging code</h2>
-    <p v-if="error">
-      {{ error }}
-    </p>
-    <v-progress-circular v-else indeterminate />
-  </main>
+  <v-container class="fill-height">
+    <div class="ma-auto d-flex flex-column align-center">
+      <h2>Exchanging code</h2>
+      <p v-if="error">
+        {{ error }}
+      </p>
+      <v-progress-circular v-else indeterminate class="mt-4" />
+    </div>
+  </v-container>
 </template>
