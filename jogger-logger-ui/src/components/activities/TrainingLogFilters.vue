ad1b024e4769201194035143934b2a153d63c200
Lynnsey Martin <lmartin@hubspot.com>
2023-06-25 22:26:20 -0400

    add menu components

--- jogger-logger-ui/src/components/activities/TrainingLogFilters.vue
+++ jogger-logger-ui/src/components/activities/TrainingLogFilters.vue
@@ -1,26 +1,62 @@
 <script lang="ts">
+const MENU_WIDTH = 300;
+
 export default {
   data() {
-    return { collapse: true };
+    return { collapse: true, MENU_WIDTH };
   },
   methods: {
-    toggleToolbar() {
-      this.collapse = !this.collapse;
+    openMenu() {
+      this.collapse = false;
+    },
+    closeMenu() {
+      this.collapse = true;
+    },
+    include() {
+      return [document.querySelector('.filters-toggle')];
     },
   },
 };
 </script>
 
 <template>
-  <v-btn class="training-log-filters" icon="mdi-menu-right" size="40" />
+  <div class="filters-toolbar">
+    <v-btn
+      class="filters-toggle"
+      :icon="`mdi-menu-${collapse ? 'right' : 'left'}`"
+      size="40"
+      @click="collapse = !collapse"
+    />
+    <v-expand-x-transition>
+      <v-card
+        v-show="!collapse"
+        v-click-outside="{ handler: closeMenu, include }"
+        :width="MENU_WIDTH"
+        class="filters-menu h-100"
+      />
+    </v-expand-x-transition>
+  </div>
 </template>
 
 <style scoped>
-.training-log-filters {
+.filters-toolbar {
+  z-index: 1;
   position: fixed;
+  left: 0;
+}
+
+.filters-toggle {
+  z-index: 2;
+  position: fixed;
+  top: 10%;
   border-top-left-radius: 0;
   border-bottom-left-radius: 0;
-  top: 10%;
+}
+
+.filters-menu {
+  z-index: 1;
+  position: fixed;
+  top: 0;
   left: 0;
 }
 </style>
